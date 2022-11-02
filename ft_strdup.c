5334b3e9ab0e3215e74e09b565a1722bedc16a42
kvebers <kvebers@student.42heilbronn.de>
2022-11-02 16:39:27 +0100

    Small Optimisation Of the Code

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 11:13:55 by kvebers           #+#    #+#             */
-/*   Updated: 2022/11/01 17:52:53 by kvebers          ###   ########.fr       */
+/*   Updated: 2022/11/02 16:31:01 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strdup(const char *s1)
 	counter1 = 0;
 	while (*(s1 + counter) != '\0')
 		counter++;
-	dest = malloc((counter + 1) * sizeof(char));
+	dest = ft_calloc((counter + 1), sizeof(char));
 	if (dest == NULL)
 		return (NULL);
 	while (counter1 < counter)
@@ -30,6 +30,5 @@ char	*ft_strdup(const char *s1)
 		*(dest + counter1) = *(s1 + counter1);
 		counter1++;
 	}
-	*(dest + counter) = '\0';
 	return (dest);
 }
