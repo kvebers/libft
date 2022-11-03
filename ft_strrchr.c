3135f33d16651295f4b2736e3ab890ac7d701fa5
kvebers <kvebers@student.42heilbronn.de>
2022-11-03 01:24:25 +0100

    Edge cases + C%256

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 14:07:07 by kvebers           #+#    #+#             */
-/*   Updated: 2022/10/22 13:59:47 by kvebers          ###   ########.fr       */
+/*   Updated: 2022/11/02 23:35:04 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,20 @@ char	*ft_strrchr(const char *s, int c)
 	temp = 0;
 	cnt = 0;
 	isfound = 0;
+	if (*ptr == '\0' && (c % 256) == '\0')
+		return (ptr);
 	while (*(ptr + cnt) != '\0')
 	{
-		if (*(ptr + cnt) == c)
+		if (*(ptr + cnt) == (c % 256))
 		{
 			temp = cnt;
 			isfound = 1;
 		}
 		cnt++;
 	}
-	if (*(ptr + cnt) == c)
+	if (*(ptr + cnt) == (c % 256))
 		temp = cnt;
-	if (temp > 0)
-		return (ptr + temp);
-	if (temp == 0 && isfound == 1)
+	if ((temp == 0 && isfound == 1) || temp > 0)
 		return (ptr + temp);
 	return (NULL);
 }
